136f7d78d457740511e3880a70d142b8cd4f3c0a
VoidXpulse <48094071+VoidXpulse@users.noreply.github.com>
2019-11-09 13:06:49 -0500

    Adding Upgrade Button

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var treenum = 0;
+var lifetimetrees = 0;
+var treeClickMult = 0;
+var treeUpgrade = false;
+var treeUpgradeOneAmount = 0;
 
 function treeClick(){ //will add one to treenum if tree.png is clicked
-  treenum++;
+  treenum = treenum + treeClickValue;
+  lifetimetrees = lifetimetrees + 1 +treeUpgradeOneAmount;
   document.getElementById('treeNumDisplay').innerHTML = "You have planted " + treenum + " trees.";
+}
+
+function treeUpgradeOne() {
+  treenum = treenum - (treeUpgradeOneAmount * 1);
+  treeUpgradeOne++;
 }
