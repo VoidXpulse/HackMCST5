9b44efa605049f6f7bef217ec0ce13b9e4cdfbe2
VoidXpulse <48094071+VoidXpulse@users.noreply.github.com>
2019-11-09 12:02:32 -0500

    removing irrelevant things.

--- script.js
+++ script.js
@@ -3,6 +3,3 @@ var treenum = 0;
 function treeClick(){ //will add one to treenum if tree.png is clicked
   treenum++;
 }
-
-var treePng = document.getElementById('treepng'); 
-treePng.addEventListener('click', clickHandler);
