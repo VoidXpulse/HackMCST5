e87b0f2643af4443efaf1ef92108b91f0823ee36
VoidXpulse <48094071+VoidXpulse@users.noreply.github.com>
2019-11-09 12:37:13 -0500

    fixing treedisplay

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ var treenum = 0;
 
 function treeClick(){ //will add one to treenum if tree.png is clicked
   treenum++;
+  document.getElementById('treeNumDisplay').innerHTML = "You have planted " + treenum + " trees.";
 }
