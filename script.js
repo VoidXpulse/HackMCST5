377274434cb1edd420aa30085a863b4103ec420e
VoidXpulse <48094071+VoidXpulse@users.noreply.github.com>
2019-11-09 13:34:31 -0500

    fixing tree upgrade. AGAIN.

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ var treenum = 0;
 var lifetimetrees = 0;
 var treeClickMult = 0;
 var treeUpgrade = false;
-var treeUpgradeOneAmount = 0;
+var treeUpgradeOneAmount = 1;
 
 function treeClick(){ //will add one to treenum if tree.png is clicked
   treenum = treenum + 1 + treeUpgradeOneAmount;
-  lifetimetrees = lifetimetrees + 1 + treeUpgradeOneAmount;
+  lifetimetrees = lifetimetrees + treeUpgradeOneAmount;
   document.getElementById('treeNumDisplay').innerHTML = "You have planted " + treenum + " trees.";
 }
 
 function treeUpgradeOne() { //adds one to tree UpgradeAmount in exchange for trees.
-  if(treenum >= treeUpgradeOneAmount){
-    treenum = treenum - (treeUpgradeOneAmount * 1);
+  if(treenum >= treeUpgradeOneAmount * 10){
+    treenum = treenum - (treeUpgradeOneAmount * 10);
     treeUpgradeOneAmount++;
     document.getElementById('treeNumDisplay').innerHTML = "You have planted " + treenum + " trees.";
   }
